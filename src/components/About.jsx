44f92db3dcbe06471c614d2dbadb57a3715823ac
Nihad Balakisiyev <nihadbalakisiyev18@gmail.com>
2023-07-11 00:17:43 +0400

    added framer motion animation and updated cv and about content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import cv from "../CV.pdf";
+import { motion } from "framer-motion";
+import cv from "../Nihad-Balakishiyev-CV.pdf";
 function About() {
-      <Helmet>
-        <title>About</title>
-      </Helmet>
+  <Helmet>
+    <title>About</title>
+  </Helmet>;
   return (
-    <div className='about-container'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className='about-container'
+    >
       <div className='row'>
         <h1 className='header'>About me</h1>
         <p className='about-text'>
-          My name is Nihad. In 2020, I entered the Azerbaijan Technical
-          University with a degree in Computer Science. I am learning front end
-          programming because of my interest in the web programming. I am open
-          to new job offers.
+          My name is Nihad. In 2020, I entered the Faculty of Computer Sciences
+          of Azerbaijan Technical University. As I am interested in web
+          programming, I am learning frontend programming. I have 1 real
+          project, this is the site of{" "}
+          <a
+            style={{ color: "#0ea5e9", borderBottom: "1px solid white" }}
+            href='https://kovrolin.az/'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            Kovrolin.az
+          </a>{" "}
+          company I am open to new job offers.
         </p>
         <a
           href={cv}
           target='_blank'
-          download='Nihad-Balakishiyev-CV'
+          download='Nihad-Balakisiyev-CV'
           className='buttonDownload'
           rel='noopener noreferrer'
         >
@@ -28,7 +42,7 @@ function About() {
           </span>
         </a>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
