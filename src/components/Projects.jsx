ab10cc786e37eaece8dc9f153981f1085e570fd6
Nihad Balakisiyev <nihadbalakisiyev18@gmail.com>
2023-07-11 00:21:20 +0400

    added framer motion animation and added Kovrolin.az project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Project from "./Project";
 import { Helmet } from "react-helmet";
+import { motion } from "framer-motion";
 //!------Images--------------------------------
+import Kovrolin from "../images/kovrolin.png";
 import WeatherApp from "../images/Weather.png";
 import BrandColor from "../images/BrandColorsProjectImage.png";
 import SpendMoneyGame from "../images/SpendMoneyGameImage.png";
@@ -12,7 +14,31 @@ import InteriorConsultant from "../images/InteriorConsultant.png";
 import NotFoundPage from "../images/NotFoundPage.png";
 //!----------------------------------------------
 function Projects() {
+  const container = {
+    visible: {
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const projectItem = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
   let projectsData = [
+    {
+      image: Kovrolin,
+      tag: "#React #Tailwind CSS #i18next",
+      title: "Kovrolin.az ",
+      description:
+        "The main purpose of this site is to present the products of Kovrolin.az. The site consists of 7 pages: home, about, services, gallery, etc. The site is available in 3 languages: Azerbaijani,English and Russian",
+      demoLink: "https://kovrolin.az/",
+    },
     {
       image: WeatherApp,
       tag: "#React #Redux #Tailwind.css",
@@ -25,7 +51,8 @@ function Projects() {
       image: BrandColor,
       tag: "#React #SCSS",
       title: "BrandColors Clone ",
-      description: "The biggest collection of official brand color codes around.",
+      description:
+        "The biggest collection of official brand color codes around.",
       demoLink: "https://brandcolors-clone.netlify.app/",
       codeLink: "https://github.com/Nihad18/react-brandcolors-clone",
     },
@@ -33,7 +60,8 @@ function Projects() {
       image: SpendMoneyGame,
       tag: "#React #CSS",
       title: "Spend money game ",
-      description: "You can spend a lot of money on this site. The products you buy are added to the cart, you can delete them from the cart if you want.",
+      description:
+        "You can spend a lot of money on this site. The products you buy are added to the cart, you can delete them from the cart if you want.",
       demoLink: "https://spend-money-game.netlify.app/",
       codeLink: "https://github.com/Nihad18/Spend-Bill-Gates-Money-Game",
     },
@@ -81,25 +109,32 @@ function Projects() {
       demoLink: "https://epic-agnesi-ad1998.netlify.app/",
       codeLink:
         "https://github.com/Nihad18/Devchallenges.io-Responsive-Web-Development/tree/main/404-not-found-master",
-      },
+    },
   ];
-      <Helmet>
-        <title>Projects</title>
-      </Helmet>
+  <Helmet>
+    <title>Projects</title>
+  </Helmet>;
   return (
-    <div className='project-container'>
+    <motion.div
+      initial='hidden'
+      animate='visible'
+      variants={container}
+      className='project-container'
+    >
       {projectsData.map((item, idx) => (
-        <Project
-          key={idx}
-          image={item.image}
-          tag={item.tag}
-          title={item.title}
-          description={item.description}
-          demoLink={item.demoLink}
-          codeLink={item.codeLink}
-        />
+        <motion.div variants={projectItem}>
+          <Project
+            key={idx}
+            image={item.image}
+            tag={item.tag}
+            title={item.title}
+            description={item.description}
+            demoLink={item.demoLink}
+            codeLink={item.codeLink}
+          />
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 }
 
